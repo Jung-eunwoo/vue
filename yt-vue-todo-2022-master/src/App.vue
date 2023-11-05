660c4184221d781efc9ff5a9315f1907fe1289d0
정은우 <wjddmsdn_8976@naver.com>
2023-11-05 13:17:56 +0900

    App.vue: 할일 삭제, 할일 추가 후 값 리셋

--- yt-vue-todo-2022-master/src/App.vue
+++ yt-vue-todo-2022-master/src/App.vue
@@ -9,66 +9,92 @@ const input_category = ref(null)
 
 
 const todos_asc = computed(() => todos.value.sort((a, b) => {
-	return b.createAt - a.createAt
+    return b.createAt - a.createAt
 }))
 const addTodo = () => {
-	if (input_content.value.trim() === '' || input_category.value === null) {
-		return 
-	}
-	console.log('addTodo')
+    if (input_content.value.trim() === '' || input_category.value === null) {
+        return
+    }
+    console.log('addTodo')
 
-	todos.value.push({
-		content: input_content.value,
-		category: input_category.value,
-		done: false,
-		createAt: new Data.getTime()
-	})
-// push만 해서는 watch에 감지되지 않음
-// 내부에 추가되는거지 문법상 todos라는 변수가 변화된 것으로 쳐지지 않기 때문에
-	todo
+    todos.value.push({
+        content: input_content.value,
+        category: input_category.value,
+        done: false,
+        createAt: Date.now()
+    })
+    // push만 해서는 watch에 감지되지 않음
+    // 내부에 추가되는거지 문법상 todos라는 변수가 변화된 것으로 쳐지지 않기 때문에
+    // watch에 감지되게 하는 유일한 방법은
+    // (변수명 여기선 todos).value = blah blah
+    input_content.value= ''
+    input_category.value = null
+}
+
+const removeTodo = todo => {
+    // 해당코드 이해 필요
+    todos.value = todos.value.filter(t => t !== todo )
 }
 
 watch(todos, newVal => {
-	localStorage.setItem('todos',JSON.stringify(newVal))
-})
+    localStorage.setItem('todos', JSON.stringify(newVal))
+}, { deep: true })
 
 watch(name, (newVal) => {
-	localStorage.setItem('name', newVal)
+    localStorage.setItem('name', newVal)
 })
 
 onMounted(() => {
-	name.value = localStorage.getItem('name') || ''
+    name.value = localStorage.getItem('name') || ''
+    todos.value = JSON.parse(localStorage.getItem('todos') || [])
 })
 
 </script>
 <template>
-	<main class="app">
-		<section class="greating">
-			<h2 class="title">
-				What'up, <input type="text" placeholder="Name here" v-model="name">
-			</h2>
-		</section>
-		<section class="create-todo">
-			<h3>CREATE A TODO</h3>
-			<form @submit.prevent="addTodo">
-				<h4>What's on your todo list?</h4>
-				<input type="text" placeholder="e.g. make a video" v-model="input_content">
+    <main class="app">
+        <section class="greating">
+            <h2 class="title">
+                What'up, <input type="text" placeholder="Name here" v-model="name">
+            </h2>
+        </section>
+        <section class="create-todo">
+            <h3>CREATE A TODO</h3>
+            <form @submit.prevent="addTodo">
+                <h4>What's on your todo list?</h4>
+                <input type="text" placeholder="e.g. make a video" v-model="input_content">
 
-				<h4>Pick a category</h4>
-				<div class="options">
-					<label>
-						<input type="radio" name="category" value="Business" v-model="input_category">
-						<span class="bubble business"></span>
-						<div>Business</div>
-					</label>
-					<label>
-						<input type="radio" name="category" value="Personal" v-model="input_category">
-						<span class="bubble Personal"></span>
-						<div>Personal</div>
-					</label>
-				</div>
-				<input type="submit" value="Add todos">
-			</form>
-		</section>
-	</main>
+                <h4>Pick a category</h4>
+                <div class="options">
+                    <label>
+                        <input type="radio" name="category" value="Business" v-model="input_category">
+                        <span class="bubble business"></span>
+                        <div>Business</div>
+                    </label>
+                    <label>
+                        <input type="radio" name="category" value="Personal" v-model="input_category">
+                        <span class="bubble Personal"></span>
+                        <div>Personal</div>
+                    </label>
+                </div>
+                <input type="submit" value="Add todos">
+            </form>
+        </section>
+        <section class="todo-list">
+            <h3>TODO LIST</h3>
+            <div class="list">
+                <div v-for="todo in todos_asc" :class="`todo-item ${todo.done && 'done'}`">
+                    <label>
+                        <input type="checkbox" v-model="todo.done"/>
+                        <span :class="`bubble ${todo.category}`"></span>
+                    </label>
+                    <div class="todo-content">
+                        <input type="text" v-model="todo.content">
+                    </div>
+                    <div class="actions">
+                        <button class="delete" @click="removeTodo(todo)">Delete</button>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
 </template>
