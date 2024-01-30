ab9b32dca89dce94c2ae2c64ac4ea41f4e452b2c
정은우 <136785138+Jung-eunwoo@users.noreply.github.com>
2024-01-31 00:27:38 +0900

    feat/vue3-elementalplust-test

---
+++ test/vue-component-test/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div>
+      <p>
+        If Element Plus is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+    </div>
+    <el-row class="mb-4" style="display:flex; justify-content:center">
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </el-row>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  display: flex;
+  justify-content: center;
+}
+</style>
---
+++ test/vue-component-test/src/components/CpIconCircleButton.vue
@@ -0,0 +1,107 @@
+<!-- 
+    240129 : CpIconCircleButton.vue
+    정은우 
+    기존 CpButton을 기반으로 만들고 있으나 아직 고치는 중.. ^ㅡㅠ
+ -->
+<template>
+    <el-button
+    circle
+    :class="classArr"
+    :style="innerStyle"
+    :disabled="disabled"
+    @click="$emit('click')"
+  >
+    <div>
+      <slot />
+    </div>
+  </el-button>
+</template>
+
+<script setup lang="ts">
+import { defineProps, defineEmits, computed } from "vue";
+import { ElButton } from "element-plus";
+
+const props = withDefaults(
+  defineProps<{
+    type: "outlined" | "solid" | "icon";
+    classArr?: string[];
+    bgColor?: string;
+    borderRadius?: string;
+    textColor?: string;
+    hoverColor?: string;
+    width?: string;
+    height?: string;
+    disabled?: boolean;
+  }>(),
+  {
+    classArr: () => ["cp-text-title-2"],
+    bgColor: "var(--cp-color-red)",
+    borderRadius: "var(--cp-number-12)",
+    textColor: "var(--cp-color-grey-100)",
+    hoverColor: "var(--cp-color-red-light)",
+    width: "337px",
+    height: "55px",
+    disabled: false,
+  },
+);
+defineEmits<{
+  (e: "click"): void;
+}>();
+
+const innerStyle = computed(() => {
+  const common = {
+    "--el-border-radius-base": props.borderRadius,
+    width: props.width,
+    height: props.height,
+  };
+
+  switch (props.type) {
+    case "solid": {
+      return {
+        ...common,
+        "--el-button-text-color": props.textColor,
+        "--el-button-hover-text-color": props.textColor,
+        "--el-button-active-text-color": props.textColor,
+        "--el-button-bg-color": props.bgColor,
+        "--el-button-hover-bg-color": props.hoverColor,
+        "--el-button-active-bg-color": props.hoverColor,
+        "--el-button-hover-color": props.bgColor,
+        "--el-button-active-border-color": props.hoverColor,
+        "--el-button-hover-border-color": props.hoverColor,
+      };
+    }
+    case "outlined": {
+      return {
+        ...common,
+        "--el-button-text-color": props.bgColor,
+        "--el-button-hover-text-color": props.hoverColor,
+        "--el-button-active-text-color": props.hoverColor,
+        "--el-button-bg-color": "transparent",
+        "--el-button-hover-bg-color": "transparent",
+        "--el-button-active-bg-color": "transparent",
+        "--el-button-border-color": props.bgColor,
+        "--el-button-active-border-color": props.hoverColor,
+        "--el-button-hover-border-color": props.hoverColor,
+      };
+    }
+    case "icon": {
+      return {
+        ...common,
+        "--el-button-text-color": props.textColor,
+        "--el-button-hover-text-color": props.textColor,
+        "--el-button-active-text-color": props.textColor,
+        "--el-button-bg-color": "transparent",
+        "--el-button-hover-bg-color": "transparent",
+        "--el-button-active-bg-color": "transparent",
+        "--el-button-border-color": "transparent",
+        "--el-button-active-border-color": "transparent",
+        "--el-button-hover-border-color": "transparent",
+        padding: "0",
+      };
+    }
+    default: {
+      return {};
+    }
+  }
+});
+</script>
