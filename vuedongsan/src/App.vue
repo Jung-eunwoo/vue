c31125de19b602b49c93d4664ff56fdb11e3f4d3
정은우 <136785138+Jung-eunwoo@users.noreply.github.com>
2024-06-09 18:39:04 +0900

    update data and modal

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -4,12 +4,13 @@
     <input type="text" v-model="valueModel"/>
   </div>
   <!-- v-if="조건식" : 조건식이 참 일때만 HTML 보여줌-->
-  <div @click="isOpenModal = !isOpenModal">
+  <div @click="isOpenModal = false">
     <div class="black-bg" v-if="isOpenModal === true">
       <div class="white-bg">
         <h4>상세페이지</h4>
         <p>상세페이지 내용임</p>
       </div>
+      <button @click="isOpenModal= false">close</button>
     </div>
   </div>
 
@@ -18,21 +19,21 @@
   <!-- vue의 반복문 -->
   <!-- <태그 v-for="작명 in 몇회" :key="" -->
   <div class="menu">
-    <a v-for="작명 in menus" :key="작명">{{ 작명 }}</a>
+    <a v-for="i in menus" :key="i">{{ i }}</a>
   </div>
-  <div v-for="(product, i) in products" :key="i">
-    <h4 @click="isOpenModal = !isOpenModal">{{ product.name }}</h4>
+  <div v-for="(product, i) in onerooms" :key="i" class="item-wrap">
+    <img :src="product.image" alt="product image" class="img-container"/>
+    <h4 @click="isOpenModal = true">{{ product.title }}</h4>
     <p>{{ product.price }} 만원</p>
     <!-- @ == v- 의 약자 -->
-    <button @click="product.reportNum += 1">허위매물신고</button><span>신고수 : {{ product.reportNum }}</span>
+    <!-- <button @click="product.reportNum += 1">허위매물신고</button><span>신고수 : {{ product.reportNum }}</span> -->
   </div>
 
   
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-// import oneroomdata from './assets/oneroom'
+import data from './assets/oneroom.js'
 
 
 export default {
@@ -42,10 +43,14 @@ export default {
       // 데이터 보관함 : 저장하고 싶은 변수들을 다 담아놓으면 됨
       // HTML 속성도 데이터 바인딩 가능
       // :(콜론)style="데이터이름" 으로 지정
+      onerooms:data,
       valueModel:'안녕뭐가뭔지모르겠어..',
       isOpenModal: false,
       menus: ['Home', 'Shop', 'About'],
-      products: [{ name: '역삼동원룸', price: 50, reportNum: 0 }, { name: '하삼동원룸', price: 60, reportNum: 0 }, { name: '수완동원룸', price: 80, reportNum: 0 }],
+      products: [
+        { name: '역삼동원룸', price: 50, reportNum: 0, imgSrc:require('./assets/room0.jpg') },
+        { name: '하삼동원룸', price: 60, reportNum: 0, imgSrc: require('./assets/room1.jpg') },
+        { name: '수완동원룸', price: 80, reportNum: 0, imgSrc:require('./assets/room2.jpg') }],
     }
   },
   // vue에서 함수 만들고 싶으면 method에다가 다 몰빵
@@ -75,7 +80,6 @@ div {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
 .menu {
@@ -102,4 +106,16 @@ div {
   background-color: white;
   border-radius: 0.5rem;
   padding: 20px;
-}</style>
+}
+.item-wrap{
+  width:100%;
+  padding: 0 40%;
+
+}
+.img-container{
+  width: 100%;
+  margin-top:40px;
+
+}
+
+</style>
